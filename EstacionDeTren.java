c6e8225885f1085275361f0bdbcde495756f0bfc
Brian <nausikachampion69@gmail.com>
2016-10-21 13:22:40 +0200

    0178.b añadido metodo de simulacion de venta

---
+++ EstacionDeTren.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class TrainStation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EstacionDeTren
+{
+    private MaquinaTickets maquina1;
+    private MaquinaTickets maquina2;
+    private int totalDinero;
+
+    public EstacionDeTren()
+    {
+        maquina1 = new MaquinaTickets(300);
+        maquina2 = new MaquinaTickets(200);
+        totalDinero = 0;
+  }
+  
+  public void imprimirDineroTotal()
+    {
+        totalDinero = maquina1.getTotal();
+        totalDinero = totalDinero + maquina2.getTotal();
+        System.out.println(totalDinero);
+    }
+  public void simulateSellTickets()
+    {
+        maquina1.insertDinero(300);
+        maquina2.insertDinero(200);
+        maquina1.printTicket();
+        maquina2.printTicket();
+    }
+}
